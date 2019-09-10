24c5649b9e9791c35a6ed176f491eb9f05520479
Eric Loudon <loudon.eric@gmail.com>
2019-09-10 13:02:40 +0000

    Automatically backed up by Learn

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -1,17 +1,22 @@
 class MusicImporter
-  attr_accessor :path, :song, :artist, :genre, :musiclibrarycontroller
-  def initialize(path)
-    @path = path
-  end
 
-  def files
-    Dir.entries(@path).select! {|entry| entry.end_with?(".mp3")}
-  end
+  attr_accessor :path
 
-  def import
-    self.files.each do |filename|
-      Song.create_from_filename(filename)
+  list_of_filenames = []
+
+    def initialize(path)
+      @path = path
     end
-  end
 
-end
+      def files
+        Dir.entries(path).select do | file | !File.directory? file
+      end
+
+    end
+
+    def import
+      files.each{ |filename| Song.create_from_filename(filename) }
+    end
+
+
+  end
