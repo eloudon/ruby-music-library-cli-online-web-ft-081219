dce7ba50773f6ae222fb7f896088218349813caa
Eric Loudon <loudon.eric@gmail.com>
2019-09-10 13:03:38 +0000

    Automatically backed up by Learn

--- lib/Artist.rb
+++ lib/Artist.rb
@@ -1,8 +1,7 @@
-require 'pry'
-
 class Artist
-  attr_accessor :name, :song, :musiclibrarycontroller, :musicimporter
   extend Concerns::Findable
+
+  attr_accessor :name, :songs, :genres
   @@all = []
 
   def initialize(name)
@@ -10,52 +9,39 @@ class Artist
     @songs = []
   end
 
-  def self.all
-    @@all
-  end
-
-  def self.destroy_all
-    @@all.clear
-  end
-
-  def save
-    @@all << self
-  end
-
-  def self.create(artist)
-    artist = self.new(artist)
-    artist.save
-    artist
+  def genres
+    @songs.map { |song| song.genre }.uniq
   end
 
   def songs
     @songs
   end
 
+    def save
+      @@all << self
+    end
+
+    def self.create(name)
+      artist = Artist.new(name)
+
+      @@all << artist
+      artist
+      end
+
+    def self.all
+    @@all
+    end
+
+    def self.destroy_all
+      @@all.clear
+    end
+
   def add_song(song)
-    if song.artist == nil
-      song.artist = self
-    else
-      nil
-    end
-    if @songs.include?(song)
-      nil
-    else
-      @songs << song
-    end
-    song
+
+      song.artist = self unless song.artist == self
+    @songs.push(song) unless @songs.include?(song)
+
   end
 
-  def genres
-    @new_array = []
-    @songs.each do |song|
-      if @new_array.include?(song.genre)
-        nil
-      else
-        @new_array << song.genre
-      end
-    end
-    @new_array
-  end
 
 end
